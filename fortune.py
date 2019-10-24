cbf893895aab27342021b9f7bc9ec5e5b2ceb0cf
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-10-24 14:40:03 -0700

    changed html

---
+++ fortune.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Renders the home page with links to Fortune and Weather."""
+    return render_template('index.html')
+    
