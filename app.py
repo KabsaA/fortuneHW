f07ba2235fb40d8c36ab6183e16c92fabd413e36
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-11-11 18:30:44 -0800

    hw 2

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    """Renders the home page with links to Fortune."""
+    return render_template('index.html')
+
+@app.route('/fortune')
+def fortune_form():
+    """Renders the fortune form page."""
+    return render_template('fortune.html')
+
+@app.route('/fortune_result')
+def fortune_result():
+    # users_bday = request.args.get('month')
+    users_name = request.args.get('name')
+    users_month = request.args.get('month')
+    drink = request.args.get('tell_fortune')
+
+    if drink == "1" and ("E" or "e" in users_name):
+        fortune = "You'll experience a magical day!"
+    elif len(users_name) >= 0 and drink != "1" and users_month == "1":
+        fortune= "Your day will suprise you!"
+    elif users_month == "2" or users_month == "3" and drink != "1":
+        fortune= "You will have a wonderful day!"
+    else:
+        fortune= "You will have a terrific day!"
+
+    return render_template('fortune_result.html', fortune=fortune)
--- fortune.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    """Renders the home page with links to Fortune and Weather."""
-    return "page"
